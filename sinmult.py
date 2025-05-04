4b8a070870fff79cf2025365659a0da6db927d89
hosboglot <ms581654@yandex.ru>
2025-05-04 18:09:48 +0300

    sinmult changes

--- sinmult.py
+++ sinmult.py
@@ -4,9 +4,12 @@ import pyqtgraph as pg
 
 from pgplotter import Plotter, Line
 
-# ffmpeg.exe -r 30 -i frames/frame_%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p output.mp4
+# ffmpeg.exe -r 60 -i frames/frame_%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p output.mp4
 
-plotter = Plotter((-0.5, 15, -3, 3), False)
+plotter = Plotter(
+    (-0.5, 15, -3, 3),
+    export=True
+)
 
 lim_f = lambda t: np.exp(-0.1 * (t // np.pi) * np.pi)
 data = [
@@ -18,7 +21,8 @@ colors = ['b', 'g', 'r']  # Разные цвета для графиков
 
 plotter.add_line(zero_line := Line(
     np.arange(-1.5, 0, 0.02),
-    lambda x: np.zeros_like(x)
+    lambda x: np.zeros_like(x),
+    point=True, pen=pg.mkPen('orange', width=3)
 ))
 for i, line in enumerate(data):
     plotter.add_line(Line(
